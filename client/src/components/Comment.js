f8ed3aa8bd43aeede6c2f657d30725fa631f57a8
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-10-31 10:24:53 +0000

    comments set up

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -1,3 +1,31 @@
-export function Comment({ id, message }) {
-  return <div>{message}</div>;
+import { IconBtn } from "./IconBtn";
+import { FaComment, FaEdit, FaHeart, FaReply, FaTrash } from "react-icons/fa";
+
+const dateFormatter = new Intl.DateTimeFormat(undefined, {
+  dateStyle: "medium",
+  timeStyle: "short",
+});
+
+export function Comment({ id, message, user, createdAt }) {
+  return (
+    <>
+      <div className="comment">
+        <div className="header">
+          <span className="name">{user.name}</span>
+          <span className="date">
+            {dateFormatter.format(Date.parse(createdAt))}
+          </span>
+          <div className="message">{message}</div>
+          <div className="footer">
+            <IconBtn Icon={FaHeart} aria-label="Like">
+              2
+            </IconBtn>
+            <IconBtn Icon={FaReply} aria-label="Reply" />
+            <IconBtn Icon={FaEdit} aria-label="Edit" />
+            <IconBtn Icon={FaTrash} aria-label="Delete" color="danger" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
