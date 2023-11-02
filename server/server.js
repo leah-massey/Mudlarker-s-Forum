13b0b362ac3b65fd529ea83c803dfbd12a7c04fb
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-11-02 10:12:00 +0000

    server side set up for 'like' feature

--- server/server.js
+++ server/server.js
@@ -44,6 +44,7 @@ const COMMENT_SELECT_FIELDS = {
   },
 };
 
+//retrieve all posts
 app.get("/posts", async (req, res) => {
   return await commitToDb(
     prisma.post.findMany({
@@ -55,43 +56,78 @@ app.get("/posts", async (req, res) => {
   );
 });
 
+// retrieve a specific post
 app.get("/posts/:id", async (req, res) => {
   return await commitToDb(
-    prisma.post.findUnique({
-      where: {
-        id: req.params.id,
-      },
-      select: {
-        body: true,
-        title: true,
-        comments: {
-          orderBy: {
-            createdAt: "desc",
-          },
-          select: COMMENT_SELECT_FIELDS,
+    prisma.post
+      .findUnique({
+        where: {
+          id: req.params.id,
         },
-      },
-    })
+        select: {
+          body: true,
+          title: true,
+          comments: {
+            orderBy: {
+              createdAt: "desc",
+            },
+            select: {
+              ...COMMENT_SELECT_FIELDS,
+              _count: { select: { likes: true } },
+            },
+          },
+        },
+      })
+      .then(async (post) => {
+        const likes = await prisma.like.findMany({
+          where: {
+            userId: req.cookies.userId,
+            commentId: { in: post.comments.map((comment) => comment.id) },
+          },
+        });
+        return {
+          ...post,
+          comments: post.comments.map((comment) => {
+            const { _count, ...commentFields } = comment;
+            return {
+              ...commentFields,
+              likedByMe: likes.find((like) => like.commentId === comment.id),
+              likeCount: _count.likes,
+            };
+          }),
+        };
+      })
   );
 });
 
+// make a comment on a specific post
 app.post("/posts/:id/comments", async (req, res) => {
   if (req.body.message === "" || req.body.message == null) {
     return res.send(app.httpErrors.badRequest("Message is required"));
   }
   return await commitToDb(
-    prisma.comment.create({
-      data: {
-        message: req.body.message,
-        userId: req.cookies.userId,
-        parentId: req.body.parentId,
-        postId: req.params.id,
-      },
-      select: COMMENT_SELECT_FIELDS,
-    })
+    prisma.comment
+      .create({
+        data: {
+          message: req.body.message,
+          userId: req.cookies.userId,
+          parentId: req.body.parentId,
+          postId: req.params.id,
+        },
+        select: COMMENT_SELECT_FIELDS,
+      })
+      .then((comment) => {
+        return {
+          ...comment,
+          // a new comment has no likes by anyone
+          likeCount: 0,
+          likedByMe: false,
+        };
+      })
   );
 });
 
+// update a comment on a specific post
 app.put("/posts/:postId/comments/:commentId", async (req, res) => {
   //error message if message body is empty
   if (req.body.message === "" || req.body.message == null) {
@@ -111,7 +147,6 @@ app.put("/posts/:postId/comments/:commentId", async (req, res) => {
       )
     );
   }
-
   return await commitToDb(
     prisma.comment.update({
       where: {
@@ -127,6 +162,7 @@ app.put("/posts/:postId/comments/:commentId", async (req, res) => {
   );
 });
 
+//delete a comment on a specific post
 app.delete("/posts/:postId/comments/:commentId", async (req, res) => {
   //identify the userId connected to the post
   const { userId } = await prisma.comment.findUnique({
